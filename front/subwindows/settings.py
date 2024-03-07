063dcdbfe7e7e9fb274bb85a3e15ee44c117e7de
hayden72177 <94942731+hayden72177@users.noreply.github.com>
2024-03-07 10:27:04 +0800

    Add language settings (bug)

--- front/subwindows/settings.py
+++ front/subwindows/settings.py
@@ -1,21 +1,46 @@
-from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout
+from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QComboBox, QLabel
 from front.interface import ApplicationInterface
+import json
+from time import sleep
 
 
 class SettingsWindow(QWidget, ApplicationInterface):
     def __init__(self):
         super(SettingsWindow, self).__init__()
 
+        self.settingsFile = None
+
+        with open("front/assets/lang/locales.json", "r") as f:
+            self.locales = json.loads(f.read())
+
+        with open("front/assets/settings.json", "r") as f:
+            self.settings = json.loads(f.read())
+
         self.onClose = False
+        self.lang = self.settings["lang"]
 
-        self.layout = QHBoxLayout()
-        self.labelButton = QPushButton()
-        self.labelButton.setObjectName("optionButton")
+        self.topLevelLayout = QHBoxLayout()
+        self.layout = QVBoxLayout()
+        self.langComboBox = QComboBox()
+        self.langComboBox.setObjectName("optionButton")
+        self.applyButton = QPushButton()
+        self.applyButton.setObjectName("optionButton")
+        self.applyButton.clicked.connect(self.applyAction)
+        self.applyButton.setText("Apply")
+        self.stateLabel = QLabel()
+        self.stateLabel.setText("")
+        self.stateLabel.setObjectName("stateLabel")
+        self.stateLabel.height = 0
 
-        self.labelButton.setText("Hello World")
+        self.langComboBox.addItems(self.locales.keys())
+        self.langComboBox.setCurrentText([i[0] for i in self.locales.items() if i[1] == self.lang][0])
 
-        self.layout.addWidget(self.labelButton)
-        self.setLayout(self.layout)
+        self.layout.addWidget(self.langComboBox)
+        self.layout.addWidget(self.applyButton)
+        self.layout.addWidget(self.stateLabel)
+        # layout bug
+        self.topLevelLayout.addLayout(self.layout)
+        self.setLayout(self.topLevelLayout)
 
         self.resize(1750, 1000)
         self.setWindowTitle("Tic Tac Toe Remastered - Settings")
@@ -27,6 +52,14 @@ class SettingsWindow(QWidget, ApplicationInterface):
     def closeEvent(self, event):
         self.onClose = True
         self.close()
+
+    def applyAction(self):
+        self.lang = self.locales[self.langComboBox.currentText()]
+        self.settings["lang"] = self.lang
+        with open("front/assets/settings.json", "w") as f:
+            f.write(json.dumps(self.settings, indent=2))
+        self.stateLabel.setText("Successful")
+        # other thread
 
 
 if __name__ == "__main__":
