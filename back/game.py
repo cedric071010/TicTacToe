a7ec0a64ac53cc3ca5e5482229656c988c71aed4
cedric071010 <cedric071010@gmail.com>
2024-03-17 20:21:26 +0800

    add comments

--- back/game.py
+++ back/game.py
@@ -7,11 +7,9 @@ class TicTacToe:
         if self.board[position] == '0':
             self.board[position] = self.current_player
             self.current_player = '2' if self.current_player == '1' else '1'
+            # simple ui for testing
         else:
             print("Invalid")
-
-    def print_board(self):
-        print(''.join(self.board) + '>')
         
     def check_win(self):
             win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
@@ -20,18 +18,27 @@ class TicTacToe:
                     return True
             return False
 
+    # simple ui for testing
+    def print_board(self):
+            print(''.join(self.board) + '>')
+
     def play(self):
         while True:
             self.print_board()
             position = int(input()) - 1
             while position < 0 or position > 8:
+                # simple ui for testing
                 print("Invalid")
+                # user input here is 1 to 9 instead of 0 to 8 for easy testing
+                # use 0 to 8 when connecting to UI
                 position = int(input()-1)
             self.make_move(position)
             if self.check_win():
+                # simple ui for testing
                 print("Player " + ('2' if self.current_player == '1' else '1') + " win")
                 break
 
+# test run
 if __name__ == "__main__":
     game = TicTacToe()
     game.play()
