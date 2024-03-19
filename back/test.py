83709043b70a495973d7ba1944d981691b6f6917
cedric071010 <cedric071010@gmail.com>
2024-03-19 13:47:28 +0800

    tested randomplayer in game

--- back/test.py
+++ back/test.py
@@ -1,8 +1,18 @@
 from game import Game
+from ai import RandomPlayer
 
 def main():
-    game_instance = Game()
-    game_instance.play()
+    game_play = Game()
+    random_player = RandomPlayer()
+
+    if not game_play.check_draw():
+        while not game_play.check_win():
+            position = random_player.make_random_move(game_play.board)
+            game_play.make_move(position)
+
+        game_play.print_board()
+        winner = 'Player 2' if game_play.current_player == '1' else 'Player 1'
+        print(f"{winner} wins")
 
 if __name__ == "__main__":
     main()
