50050089639116a706f9aa53df7a0c474d2ecb6f
cedric071010 <cedric071010@gmail.com>
2024-03-19 13:47:02 +0800

    add randomplayer

--- back/ai.py
+++ back/ai.py
@@ -1,1 +1,9 @@
+import random
 
+class RandomPlayer:
+    def __init__(self):
+        self.name = "Random Player"
+
+    def make_random_move(self, board):
+        available_positions = [i for i, cell in enumerate(board) if cell == '0']
+        return random.choice(available_positions)
