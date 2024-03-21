cb13a275934067f28cb22c9bbf4ca4c9633e2057
hayden72177 <94942731+hayden72177@users.noreply.github.com>
2024-03-21 13:46:42 +0800

    Change make_random_move to static

--- back/ai.py
+++ back/ai.py
@@ -1,9 +1,11 @@
 import random
 
+
 class RandomPlayer:
     def __init__(self):
         self.name = "Random Player"
 
-    def make_random_move(self, board):
+    @staticmethod
+    def make_random_move(board):
         available_positions = [i for i, cell in enumerate(board) if cell == '0']
         return random.choice(available_positions)
