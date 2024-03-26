46c010ae1876bbe9467f84e27089ffb56bc0e0d1
hayden72177 <94942731+hayden72177@users.noreply.github.com>
2024-03-26 13:38:16 +0800

    Add colour config

--- front/subwindows/settings.py
+++ front/subwindows/settings.py
@@ -11,6 +11,27 @@ class SettingsWindow(QWidget, ApplicationFrontInterface):
     def __init__(self):
         super(SettingsWindow, self).__init__()
 
+        self.ALL_THEMES = ['dark_amber.xml',
+                           'dark_blue.xml',
+                           'dark_cyan.xml',
+                           'dark_lightgreen.xml',
+                           'dark_pink.xml',
+                           'dark_purple.xml',
+                           'dark_red.xml',
+                           'dark_teal.xml',
+                           'dark_yellow.xml',
+                           'light_amber.xml',
+                           'light_blue.xml',
+                           'light_cyan.xml',
+                           'light_cyan_500.xml',
+                           'light_lightgreen.xml',
+                           'light_pink.xml',
+                           'light_purple.xml',
+                           'light_red.xml',
+                           'light_teal.xml',
+                           'light_yellow.xml',
+                           'default']
+
         self.settingsFile = None
 
         self.locales = ApplicationFrontInterface.readFile("front/assets/lang/locales.json")
@@ -19,6 +40,7 @@ class SettingsWindow(QWidget, ApplicationFrontInterface):
         self.onClose = False
         self.lang = self.settings["lang"]
         self.fullScreenState = self.settings["fullscreen"]
+        self.theme = self.settings["theme"]
 
         self.lineEditOnFocus = False
 
@@ -27,6 +49,7 @@ class SettingsWindow(QWidget, ApplicationFrontInterface):
         self.gridLayout = QGridLayout()
         self.langComboBox = QComboBox()
         self.fullScreenButton = QPushButton()
+        self.themeButton = QPushButton()
         self.applyButton = QPushButton()
         self.quitButton = QPushButton()
 
@@ -43,8 +66,8 @@ class SettingsWindow(QWidget, ApplicationFrontInterface):
         self.resolutionYLineEdit.returnPressed.connect(self.resolutionLineEditOnPress)
         # incomplete, needs save and link slider
 
-        ApplicationFrontInterface.setObjectID(self.langComboBox, self.fullScreenButton, self.applyButton,
-                                              self.quitButton, ID="optionButton")
+        ApplicationFrontInterface.setObjectID(self.langComboBox, self.fullScreenButton, self.themeButton,
+                                              self.applyButton, self.quitButton, ID="optionButton")
         ApplicationFrontInterface.setObjectID(self.resolutionXLineEdit,
                                               self.resolutionYLineEdit, ID="resolutionLineEdit")
 
@@ -53,7 +76,9 @@ class SettingsWindow(QWidget, ApplicationFrontInterface):
         if self.fullScreenState:
             self.showFullScreen()
 
-        ApplicationFrontInterface.assignButtons([self.applyButton, "Apply", self.applyAction],
+        ApplicationFrontInterface.assignButtons([self.themeButton, f"Theme: {self.theme.replace('.xml', '')}",
+                                                 self.themeAction],
+                                                [self.applyButton, "Apply", self.applyAction],
                                                 [self.quitButton, "Back", self.quitAction])
 
         self.langComboBox.addItems(self.locales.keys())
@@ -61,10 +86,11 @@ class SettingsWindow(QWidget, ApplicationFrontInterface):
 
         self.gridLayout.addWidget(self.langComboBox, 0, 0)
         self.gridLayout.addWidget(self.fullScreenButton, 1, 0)
-        self.gridLayout.addWidget(self.applyButton, 2, 0)
-        self.gridLayout.addWidget(self.quitButton, 3, 0)
-        self.gridLayout.addWidget(self.resolutionXLineEdit, 0, 1)
-        self.gridLayout.addWidget(self.resolutionYLineEdit, 1, 1)
+        self.gridLayout.addWidget(self.themeButton, 2, 0)
+        self.gridLayout.addWidget(self.applyButton, 3, 0)
+        self.gridLayout.addWidget(self.quitButton, 4, 0)
+        self.gridLayout.addWidget(self.resolutionXLineEdit, 5, 0)
+        self.gridLayout.addWidget(self.resolutionYLineEdit, 6, 0)
         # layout bug
         self.topLevelLayout.addLayout(self.gridLayout)
 
@@ -94,10 +120,16 @@ class SettingsWindow(QWidget, ApplicationFrontInterface):
             self.showNormal()
             self.fullScreenButton.setText("Fullscreen - Off")
 
+    def themeAction(self):
+        self.theme = self.ALL_THEMES[(self.ALL_THEMES.index(self.theme) + 1) % len(self.ALL_THEMES)]
+        self.themeButton.setText(f"Theme: {self.theme.replace('.xml', '')}")
+        # start.py integration
+
     def applyAction(self):
         self.lang = self.locales[self.langComboBox.currentText()]
         self.settings["lang"] = self.lang
         self.settings["fullscreen"] = int(self.fullScreenState)
+        self.settings["theme"] = self.theme
         ApplicationFrontInterface.writeFile("back/settings.json", self.settings)
 
     def quitAction(self):
