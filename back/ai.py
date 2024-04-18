86d862bc3803c4ad10bfedc6072e550b809ac2fb
Cedric <131578574+cedric071010@users.noreply.github.com>
2024-04-18 11:04:11 +0800

    add human ai.py

--- back/ai.py
+++ back/ai.py
@@ -1,11 +1,24 @@
 import random
 
-
 class RandomPlayer:
     def __init__(self):
         self.name = "Random Player"
 
     @staticmethod
-    def make_random_move(board):
+    def make_move(board):
         available_positions = [i for i, cell in enumerate(board) if cell == '0']
         return random.choice(available_positions)
+
+class Human:
+    def __init__(self):
+        self.name = "Human Player"
+
+    @staticmethod
+    def make_move(board):
+        available_positions = [i for i, cell in enumerate(board) if cell == '0']
+        print("available positions are " + str(available_positions))
+        move = input("enter your move> ")
+        while int(move) not in available_positions:
+            print(move + " is not available")
+            move = input("enter your move> ")
+        return int(move)
