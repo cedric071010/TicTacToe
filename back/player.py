aef72ce3213c3e9618582d6ad5c5dec1e3665d1a
Cedric <131578574+cedric071010@users.noreply.github.com>
2024-04-18 14:49:05 +0800

    rename, added human and data

---
+++ back/player.py
@@ -0,0 +1,38 @@
+import random
+
+
+class RandomPlayer:
+    def __init__(self):
+        self.name = "Random Player"
+
+    @staticmethod
+    def make_random_move(board):
+        available_positions = [i for i, cell in enumerate(board) if cell == '0']
+        return random.choice(available_positions)
+
+class Human:
+    def __init__(self):
+        self.name = "Human Player"
+
+    @staticmethod
+    def make_move(board):
+        available_positions = [i for i, cell in enumerate(board) if cell == '0']
+        print("available positions are " + str(available_positions))
+        move = input("enter your move> ")
+        while int(move) not in available_positions:
+            print(move + " is not available")
+            move = input("enter your move> ")
+        return int(move)
+
+class Data:
+    def __init__(self):
+        self.name = "Data Player"
+
+    @staticmethod
+    def make_move(board):
+        #player1
+        if board == [ '0', '0', '0', '0', '0', '0', '0', '0', '0' ]:
+
+
+        #player2
+#doing
